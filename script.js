ac8850687ce1400093a5cefb5b8cdff6dc11ac98
Kássia <kassia016@gmail.com>
2022-06-26 23:24:16 -0300

    Adjustments.

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function joinChatRoom() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usernameObject);
     promise.then(enterChatRoom);//se nome ok, entra na sala 
     promise.catch(askNewName);//se nome não ok pede outro nome
-    // console.log(promise);
 
 }
 
@@ -39,7 +38,6 @@ function onUserLogoff(error) {
 //função que permite a entarda do usuário na sala de bate papo e esconde tela de entrada
 function enterChatRoom() {
     refreshOnlineUser();
-    //alert("Nome mara");
     //esconde tela 
     const hideLoginArea = document.querySelector(".hide-login-area");
     hideLoginArea.classList.add("hidden");
@@ -48,10 +46,8 @@ function enterChatRoom() {
     chatRoomContainer.classList.remove("hidden");
 
     //mostra as mensagens 
+    showMessages();
     showMessagesIntervalId = setInterval(showMessages, 3000);
-    //recarrega as mensagens
-    //reloadMessages();
-    //Enquanto o usuário estiver na sala, a cada 5 segundos o site deve avisar ao servidor que o usuário ainda está presente, ou senão será considerado que "Saiu da sala"
 }
 
 function showMessages() {
@@ -82,10 +78,11 @@ function loadMessages(element) {
             if (element.data[i].to === username || element.data[i].to === "Todos"
                 || element.data[i].from === username) {
                 messages.innerHTML += `
-            <div class="messages-default-style private-message private-chat">
+            <div class="messages-default-style private-message">
                 <span class="text-color-gray">(${element.data[i].time})</span>
                 <span class="bold-text">${element.data[i].from}</span>
-                <span>reservadamente para</span>
+                <span> reservadamente para </span>
+                <span class="bold-text">${element.data[i].to}: </span>
                 <span>${element.data[i].text}</span>  
             </div>
             `}
@@ -103,14 +100,9 @@ function loadMessages(element) {
         }
 
     }
-    //Enquanto o usuário estiver na sala, a cada 5 segundos o site deve avisar ao servidor que o usuário ainda está presente, ou senão será considerado que "Saiu da sala"
-    // if(onlineUserIntervalId === undefined) {
-    //     onlineUserIntervalId =  setInterval(onlineUser, 5000);
-    // }
 
     //scrolla as mensagens
     const scroll = document.querySelector(".messages-container");
-    //TODO: Ler para entender o false:  https://developer.mozilla.org/pt-BR/docs/Web/API/Element/scrollIntoView#par%C3%A2metros
     scroll.scrollIntoView(false);
 }
 
@@ -141,7 +133,7 @@ function sendMessage() {
         "from": username,
         "to": recipient,
         "text": message,
-        "type": "message"
+        "type": messageStatus
     }
 
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", newMessageObject);
@@ -163,6 +155,8 @@ function sentMessage() {
 function showSidebar() {
     const sidebar = document.querySelector(".container-sidebar");
     sidebar.classList.remove("hidden");
+    const sidebarPlace = document.querySelector(".sidebar-place");
+    sidebarPlace.classList.remove("hidden");
 
     visibleUsers();
     //O site deve obter a lista de participantes assim que entra no chat e deve atualizar a lista a cada 10 segundos
@@ -179,57 +173,66 @@ function closeSidebar() {
 function visibleUsers() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
     promise.then(usersToTalk);
-    promise.catch((error)=> console.log(error));
+    promise.catch(errorSidebar);
+
 }
+
+function errorSidebar(error) {
+    console.log(error);
+}
+
+function changeMessageType(element) {
+    let isPublic = element.className === "public-chat";
+    let public = document.querySelector(".public-chat .check");
+    let private = document.querySelector(".private-chat .check");
+
+    if (isPublic) {
+        messageStatus = "message";
+        public.classList.remove("hidden");
+        private.classList.add("hidden");
+
+    } else {
+        messageStatus = "private_message";
+        public.classList.add("hidden");
+        private.classList.remove("hidden");
+    }
+}
+
+
 
 function usersToTalk(element) {
     const talkOption = document.querySelector(".all");
+    talkOption.innerHTML = `
+    <p onclick="closeSidebar(this)" class="all public-chat">
+        <img class="people" src="images\\Vector.png">Todos</p>`;
     talkOption.classList.add("chosed");
 
-    // const checkMark = document.querySelector(".check-mark");
-    // checkMark.classList.remove("hidden");
-
-
-    // talkOption.innerHTML = `
-    // <div class="users public-chat chosed" onclick="messageTo(this)">
-    // <div>
-    //     <img src="images\\Vector.png">
-    //     <span>Todos</span>
-    // </div>  
-    // 
-    // `
     for (let i = 0; i < element.data.length; i++) {
         talkOption.innerHTML += `
         <div class="users" onclick="messageTo(this)">
         <div>
             <img src="images\\Vector 2.png">
-            <span>${element.data[i].name}</span>
+            <span class="nickname">${element.data[i].name}</span>
         </div>  
         
         </div>        
         `
-    }//<img class="check-mark" src="images\\check_mark.png">
-}
-
-function reloadSidebarUsers() {
-    const username = document.querySelector(".username-box").value; //pega o valor digitado na caixa de texto
-    const usernameObject = { name: username };
-
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usernameObject);
+    }
 }
 
 function messageTo(element) {
 
-    const userChosed = document.querySelector(".users .chosed");
+    const userChosed = element.querySelector(".nickname").innerHTML;
+    
+    const textArea = document.querySelector(".textarea");
+    if(textArea !== null) {
+        textArea.focus();
+    }
 
     if (userChosed !== null) {
-        userChosed.classList.remove("chosed");
-    }
-    else {
-        element.classList.add(".users .chosed");
-
-        recipient = element.querySelector(".users span").innerHTML;
-
+        recipient = userChosed;
+    } else{
+        recipient = "Todos";
     }
 }
 
